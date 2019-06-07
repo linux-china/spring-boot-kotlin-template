e675103c300963ae836af58f44c48503860a2202
linux_china <libing.chen@gmail.com>
2019-06-06 19:19:23 -0700

    Spring Boot base testcase

---
+++ src/test/kotlin/org/mvnsearch/demo/SpringBootBaseTestCase.kt
@@ -0,0 +1,12 @@
+package org.mvnsearch.demo
+
+import org.springframework.boot.test.context.SpringBootTest
+
+/**
+ * Spring Boot base testcase
+ *
+ * @author linux_china
+ */
+@SpringBootTest
+abstract class SpringBootBaseTestCase {
+}
